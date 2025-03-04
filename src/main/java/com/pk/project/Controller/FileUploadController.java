2478a2583b817b448d20ffd36ce2d538f3c4c918
pravin sonwane <pravinson222@gmail.com>
2025-03-04 17:07:04 +0530

    File FileUploadController FileUploadHelper Updated

--- src/main/java/com/pk/project/Controller/FileUploadController.java
+++ src/main/java/com/pk/project/Controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.pk.project.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,30 +8,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.pk.project.Helper.FileUploadHelper;
+
+import java.util.logging.Logger;
+
 @RestController
 public class FileUploadController {
 
-    @SuppressWarnings("null")
+    @Autowired
+    private FileUploadHelper fileUploadHelper;
+
+    private final Logger logger = Logger.getLogger(FileUploadController.class.getName());
+
     @PostMapping("/upload")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-
-        System.out.println("File Name: " + file.getOriginalFilename());
-        System.out.println("File Type: " + file.getContentType());
-        System.out.println("File Size: " + file.getSize());
-        System.out.println("File Content: " + file.toString());
-        System.out.println("File Content: " + file.getResource());
-        System.out.println("File Content: " + file.getName());
-        System.out.println("File Content: " + file.getOriginalFilename());
-        System.out.println("File Content: " + file.getContentType());
+        logger.info("Received file: " + file.getOriginalFilename());
+        logger.info("File type: " + file.getContentType());
+        logger.info("File size: " + file.getSize());
 
         if (file.isEmpty()) {
+            logger.warning("File is empty");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please select a file to upload");
         }
 
-        if (file.getContentType().equals("image/mp4") || file.getContentType().equals("image/mkv")) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please select a valid image file to upload");
+        try {
+            boolean isUploaded = fileUploadHelper.uploadFile(file);
+            if (isUploaded) {
+                return ResponseEntity.ok("File uploaded successfully");
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong! Try again");
+            }
+        } catch (Exception e) {
+            logger.severe("Exception occurred: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong! Try again");
         }
-
-        return ResponseEntity.ok("File uploaded successfully");
     }
 }
